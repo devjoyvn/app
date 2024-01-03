18d7ed545f62f1050f863a7653ee9f14ad7b8d9f
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2024-01-02 16:15:07 -0800

    throws an error for blank strings

--- src/url-matches.spec.ts
+++ src/url-matches.spec.ts
@@ -6,16 +6,22 @@ describe('urlMatches', () => {
     expect(urlMatches(`foobar`, `foo`)).toEqual(false);
     expect(urlMatches(`o`, `foo`)).toEqual(false);
   });
-  
+
   it('requires a match for RegExp', () => {
     expect(urlMatches(`foo`, /foo/)).toEqual(true);
     expect(urlMatches(`foobar`, /foo/)).toEqual(true);
     expect(urlMatches(`o`, /foo/)).toEqual(false);
   });
-  
+
   it('matches everything with /.*/', () => {
     expect(urlMatches(`foo`, /.*/)).toEqual(true);
     expect(urlMatches(`foobar`, /.*/)).toEqual(true);
     expect(urlMatches(`o`, /.*/)).toEqual(true);
+  });
+
+  it('throws an error for blank strings', () => {
+    expect(() => {
+      urlMatches(`foo`, '');
+    }).toThrowError(/blank/);
   });
 });
--- src/url-matches.ts
+++ src/url-matches.ts
@@ -3,6 +3,11 @@ export type Matcher = string | RegExp;
 export function urlMatches(url: string, matcher: Matcher): boolean {
   switch (typeof matcher) {
     case 'string':
+      if (matcher === '') {
+        throw new Error(
+          `Invalid matcher: Tried to match '${url}' against a blank string.`
+        );
+      }
       return url === matcher;
     default:
       return matcher.test(url);
