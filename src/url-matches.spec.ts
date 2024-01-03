c447d7c682f1fd4e7cbe89ec63d84f883f8ea594
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2024-01-02 16:07:12 -0800

    wrote tests for url-matches

---
+++ src/url-matches.spec.ts
@@ -0,0 +1,21 @@
+import { urlMatches } from './url-matches';
+
+describe('urlMatches', () => {
+  it('requires an exact match for strings', () => {
+    expect(urlMatches(`foo`, `foo`)).toEqual(true);
+    expect(urlMatches(`foobar`, `foo`)).toEqual(false);
+    expect(urlMatches(`o`, `foo`)).toEqual(false);
+  });
+  
+  it('requires a match for RegExp', () => {
+    expect(urlMatches(`foo`, /foo/)).toEqual(true);
+    expect(urlMatches(`foobar`, /foo/)).toEqual(true);
+    expect(urlMatches(`o`, /foo/)).toEqual(false);
+  });
+  
+  it('matches everything with /.*/', () => {
+    expect(urlMatches(`foo`, /.*/)).toEqual(true);
+    expect(urlMatches(`foobar`, /.*/)).toEqual(true);
+    expect(urlMatches(`o`, /.*/)).toEqual(true);
+  });
+});
